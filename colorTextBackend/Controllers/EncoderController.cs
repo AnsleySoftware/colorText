33e4153b2c35a0624990930916c44bafd1c66167
Ansley Software <ansleysoftware@gmail.com>
2026-03-31 19:48:45 -0500

    dev work

---
+++ colorTextBackend/Controllers/EncoderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace colorTextBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncoderController : ControllerBase
+    {
+        [HttpPost("encode")]
+        public IActionResult Encode([FromBody] EncodeRequest request)
+        {
+            TextEncoder encoder = new TextEncoder();
+            List<string> encodedText = encoder.EncodeTextToColor(request.Input);
+            return Ok(encodedText);
+        }
+
+        [HttpPost("decode")]
+        public IActionResult DecodeColors([FromBody] DecodeRequest request)
+        {
+            TextDecoder decoder = new TextDecoder();
+            string decodedText = decoder.DecodeColorToText(request.Colors);
+            return Ok(decodedText);
+        }
+
+    }
+
+    public class EncodeRequest
+    {
+        public string Input { get; set; }
+    }
+
+    public class DecodeRequest
+    {
+        public List<string> Colors {  get; set; }
+    }
+}
---
+++ colorTextBackend/wwwroot/js/encoder.js
@@ -0,0 +1,35 @@
+const encodeButton = document.getElementById("encodeButton");
+const encodeInput = document.getElementById("encodeInput");
+const swatchDisplay = document.getElementById("swatchDisplay");
+const decodeButton = document.getElementById("decodeButton")
+const decodeInput = document.getElementById("decodeInput");
+const decodeOutput = document.getElementById("decodeOutput");
+let currentColors = [];
+encodeButton.addEventListener("click", async () => {
+    const response = await fetch("/api/encoder/encode", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ input: encodeInput.value})
+    });
+    const data = await response.json();
+    currentColors = data;
+    swatchDisplay.innerHTML = "";
+    for (const color of currentColors)
+    {
+        let colorDiv = document.createElement("div");
+        swatchDisplay.appendChild(colorDiv);
+        colorDiv.classList.add("swatch");
+        colorDiv.style.backgroundColor = color;
+    }
+
+});
+
+decodeButton.addEventListener("click", async () => {
+    const response = await fetch("/api/encoder/decode", {
+        method:"POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({colors: currentColors})
+    });
+    const data = await response.json();
+    console.log(data);
+});
