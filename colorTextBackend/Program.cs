cd7ee6b1265c037aa7da4cd9454a527481bbe805
John <ansleysoftware@gmail.com>
2026-04-04 13:20:42 -0500

    remove builder.webhost.useurls...
    due to hosting conflicts.

--- colorTextBackend/Program.cs
+++ colorTextBackend/Program.cs
@@ -2,7 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddMemoryCache();
 builder.Services.AddHttpClient();
-builder.WebHost.UseUrls("http://+:8080");
 var app = builder.Build();
 
 
